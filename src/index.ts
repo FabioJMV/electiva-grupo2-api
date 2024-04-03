f1ce95a0f5fc376e226da87448f10f3015f5ca2c
FabioJMV <109473322+FabioJMV@users.noreply.github.com>
2024-04-03 14:40:40 -0400

    Add initial project files

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
+
+import "dotenv/config";
+
+const port = Number.parseInt(process.env.PORT || "3000");
+
+const app = new Hono();
+
+// Middlewares
+app.use(cors({ origin: "*" }));
+app.use(logger());
+app.use(prettyJSON());
+
+serve(
+  {
+    fetch: app.fetch,
+    port
+  },
+  ({ port }) => console.log(`Server running on http://localhost:${port}`)
+);
