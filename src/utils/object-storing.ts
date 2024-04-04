d8594f90c800e7c4512ef4c060f69fb815370804
FabioJMV <109473322+FabioJMV@users.noreply.github.com>
2024-04-03 23:55:18 -0400

    Removing directoryExist function in object-storing.ts

--- src/utils/object-storing.ts
+++ src/utils/object-storing.ts
@@ -5,14 +5,7 @@ const imageDir = path.join(__dirname, "../../public/images");
 
 const supportedExtensions = [".jpg", ".jpeg", ".png"];
 
-function directoryExist(directory: string) {
-  if (!fs.existsSync(directory)) {
-    fs.mkdirSync(directory, { recursive: true });
-  }
-}
-
 export function getImagePath(filename: string) {
-  directoryExist(imageDir);
   for (const ext of supportedExtensions) {
     const imagepath = path.join(imageDir, `${filename}${ext}`);
 
